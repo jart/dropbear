791c019dc031515ad2a5157a10cd5a86ed7d6912
Justine Tunney <jtunney@gmail.com>
2026-03-05 09:22:05 -0800

    Refactor boxer

--- cmd/boxer/main.go
+++ cmd/boxer/main.go
@@ -36,6 +36,7 @@ var (
 	sr1             *Future
 	optionsByID     = make(map[uint32]*Option)
 	optionsByStrike = treeset.NewWith(compareOptionByStrike)
+	schwabClient    *schwab.Client
 )
 
 func main() {
@@ -45,49 +46,96 @@ func main() {
 	cash = *cashFlag
 
 	key := databento.MustLoadDefaultKey()
-	schwabClient := schwab.NewClient()
+	schwabClient = schwab.NewClient()
 
 	futureDefs := make(chan *Future, 64)
-	futureTicks := make(chan FutureTick, 64)
+	futureTicks := make(chan FutureTick, 512)
 	optionDefs := make(chan *Option, 64)
-	optionTicks := make(chan OptionTick, 64)
+	optionTicks := make(chan OptionTick, 512)
 	orderUpdates := schwabClient.OrderUpdates()
 
 	go streamFutures(key, futureDefs, futureTicks)
 	go streamOptions(key, optionDefs, optionTicks)
 
 	for {
+		// drain all pending events
 		select {
 		case f := <-futureDefs:
-			futuresByID[f.ID] = f
-			if f.Sym == symbol.Symbol('E'|'S'<<8) {
-				es = f
-			} else if f.Sym == symbol.Symbol('S'|'R'<<8|'1'<<16) {
-				sr1 = f
-				go fetchFuturePrice(key, sr1, futureTicks)
-			} else {
-				log.Fatalf("unknown future symbol: %s", f.Sym)
-			}
+			onFutureDef(key, futureTicks, f)
+			continue
 		case t := <-futureTicks:
-			future := futuresByID[t.ID]
-			if future != nil {
-				future.Bid = t.Bid
-				future.Ask = t.Ask
-				future.Price = t.Bid.Add(t.Ask).DivInt(2)
-				log.Printf("tick %s mid %s bid %s ask %s", future.Sym, future.Price, t.Bid, t.Ask)
-			}
+			onFutureTick(t)
+			continue
 		case o := <-optionDefs:
-			optionsByID[o.ID] = o
-			optionsByStrike.Add(o)
+			onOptionDef(o)
+			continue
 		case t := <-optionTicks:
-			option := optionsByID[t.ID]
-			if option != nil {
-				option.Bid = t.Bid
-				option.Ask = t.Ask
-				log.Printf("tick %s bid %s ask %s", option, t.Bid, t.Ask)
-			}
+			onOptionTick(t)
+			continue
 		case update := <-orderUpdates:
-			log.Printf("order %s: %s", update.SchwabOrderID, update.Event)
+			onOrderUpdate(update)
+			continue
+		default:
+			// all channels empty
+		}
+		makeDecisions()
+		// block until next event
+		select {
+		case f := <-futureDefs:
+			onFutureDef(key, futureTicks, f)
+		case t := <-futureTicks:
+			onFutureTick(t)
+		case o := <-optionDefs:
+			onOptionDef(o)
+		case t := <-optionTicks:
+			onOptionTick(t)
+		case update := <-orderUpdates:
+			onOrderUpdate(update)
 		}
 	}
+}
+
+func onFutureDef(key databento.ApiKey, ticks chan<- FutureTick, f *Future) {
+	futuresByID[f.ID] = f
+	if f.Sym == symbol.Symbol('E'|'S'<<8) {
+		es = f
+	} else if f.Sym == symbol.Symbol('S'|'R'<<8|'1'<<16) {
+		sr1 = f
+		go fetchFuturePrice(key, sr1, ticks)
+	} else {
+		log.Fatalf("unknown future symbol: %s", f.Sym)
+	}
+}
+
+func onFutureTick(t FutureTick) {
+	future := futuresByID[t.ID]
+	if future == nil {
+		return
+	}
+	future.Bid = t.Bid
+	future.Ask = t.Ask
+	future.Price = t.Bid.Add(t.Ask).DivInt(2)
+	log.Printf("tick %s mid %s bid %s ask %s", future.Sym, future.Price, t.Bid, t.Ask)
+}
+
+func onOptionDef(o *Option) {
+	optionsByID[o.ID] = o
+	optionsByStrike.Add(o)
+}
+
+func onOptionTick(t OptionTick) {
+	option := optionsByID[t.ID]
+	if option == nil {
+		return
+	}
+	option.Bid = t.Bid
+	option.Ask = t.Ask
+	log.Printf("tick %s bid %s ask %s", option, t.Bid, t.Ask)
+}
+
+func onOrderUpdate(update schwab.OrderUpdate) {
+	log.Printf("order %s: %s", update.SchwabOrderID, update.Event)
+}
+
+func makeDecisions() {
 }
